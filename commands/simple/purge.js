9d124584b7f0103fcabdade04a41dd539f20c609
UnlegitSenpaii <39647545+UnlegitSenpaii@users.noreply.github.com>
2018-07-26 09:57:19 +0200

    Create purge.js

---
+++ commands/simple/purge.js
@@ -0,0 +1,26 @@
+onst cmd = require("discord.js-commando");
+
+class purgecommand extends cmd.Command
+{
+    constructor(client)
+    {
+        super(client,{
+
+          name: "purge",
+          group: "simple",
+          memberName: "purge",
+          description: "deletes all messages in a channel!"
+       });
+    }
+    async run(message, args)
+    {
+    if (!message.member.hasPermission("MANAGE_MESSAGES")
+    return;
+     message.delete();
+     const fetched = await message.channel.fetchMessages();
+     message.channel.bulkDelete(fetched)
+    }
+
+}
+
+module.exports = purgecommand;
