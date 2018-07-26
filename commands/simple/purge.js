d63998a064f2ea8b1832304f55151093558ef9de
Senpaii <dashaterdas@gmail.com>
2018-07-26 10:05:21 +0200

    purgefix

--- commands/simple/purge.js
+++ commands/simple/purge.js
@@ -1,4 +1,4 @@
-onst cmd = require("discord.js-commando");
+const cmd = require("discord.js-commando");
 
 class purgecommand extends cmd.Command
 {
@@ -14,12 +14,17 @@ class purgecommand extends cmd.Command
     }
     async run(message, args)
     {
-    if (!message.member.hasPermission("MANAGE_MESSAGES")
-    return;
+    if(message.member.hasPermission("MANAGE_MESSAGES"))
+    {
      message.delete();
      const fetched = await message.channel.fetchMessages();
      message.channel.bulkDelete(fetched);
      message.channel.send("`Deleted all messages!`");
+    }
+    else 
+    {
+        message.channel.send("`You dont have enough permissions to execute this command!`")
+    }
     }
 
 }
