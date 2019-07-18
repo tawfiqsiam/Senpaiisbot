125af263fe4fb7c41b6c3d80764dff27f75aa047
Senpaii <dashaterdas@gmail.com>
2019-07-18 11:22:12 +0200

    purge fix

--- commands/simple/purge.js
+++ commands/simple/purge.js
@@ -7,15 +7,21 @@ class purgecommand extends cmd.Command {
             name: "purge",
             group: "simple",
             memberName: "purge",
-            description: "deletes all messages in a channel!"
+            description: "deletes messages in a channel!"
         });
     }
     async run(message, args) {
+        if (isNaN(args[0])) return message.channel.send("`I need to know how many messages to delete!`")
         if (message.member.hasPermission("MANAGE_MESSAGES")) {
-            message.delete();
-            const fetched = await message.channel.fetchMessages();
-            message.channel.bulkDelete(fetched);
-            message.channel.send("`Deleted all messages!`");
+            try {
+                message.channel.bulkDelete(args)
+                    .then(messages => message.channel.send("`Successfully deleted " + args[0] + " messages!`")
+                        .then(msg => msg.delete({ timeout: 10000 })));
+            }
+            catch
+            {
+                message.channel.send("`A Error occured while trying to delete!`");
+            }
         }
         else {
             message.channel.send("`You dont have enough permissions to execute this command!`")
