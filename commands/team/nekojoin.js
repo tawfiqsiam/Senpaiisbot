417b317f81d6e23e142098a58529df1e9deeecdd
tawfiqsiam <toto.siam999@gmail.com>
2020-01-14 21:33:30 +0200

    Update nekojoin.js

--- commands/team/nekojoin.js
+++ commands/team/nekojoin.js
@@ -1,22 +1,22 @@
 const cmd = require("discord.js-commando");
 const discord = require("discord.js");
-class JoinNekoCommand extends cmd.Command {
+class JoinLordCommand extends cmd.Command {
     constructor(client) {
         super(client, {
 
-            name: "neko",
+            name: "lord",
             group: "team",
-            memberName: "neko",
-            description: "Be a member of the Nekos! Nyaa~"
+            memberName: "lord",
+            description: "Be a member of the Lords!~"
         });
     }
     async run(message, args) {
-        /*      let Kitsune = message.member.guid.roles.find("name", "Kitsune");
-                let Neko = message.member.guid.roles.find("name", "Neko")
+        /*      let Kitsune = message.member.guid.roles.find("name", "Knight");
+                let Neko = message.member.guid.roles.find("name", "Lord")
                 if (Kitsune)
                 return;
                 else
-                message.member.addRole(Neko);*/
+                message.member.addRole(Lord);*/
         let logChannel = message.guild.channels.find(`name`, "logs");
         let CommandChannel = message.guild.channels.find(`name`, "commands");
 
@@ -28,16 +28,16 @@ class JoinNekoCommand extends cmd.Command {
         }
         let Neko = message.member.guild.roles.find("name", "Neko");
         // let Kitsune = message.member.guild.roles.find("name", "Kitsune");
-        if (message.member.roles.find("name", "Kitsune")) {
-            message.author.send("`You are already a Kitsune! :3`")
+        if (message.member.roles.find("name", "Knight")) {
+            message.author.send("`You are already a Kinght! :3`")
         }
         else {
-            if (message.member.roles.find("name", "Neko")) {
-                message.author.send("`You are already a Neko! Nyaa~`")
+            if (message.member.roles.find("name", "Lord")) {
+                message.author.send("`You are already a Lords! ~`")
             }
             else {
-                message.member.addRole(Neko);
-                message.author.send("`Welcome to the Nekos! Nyaa~`");
+                message.member.addRole(Lord);
+                message.author.send("`Welcome to the Lords! ~`");
                 currentNekoMembers.push(message.author);
             }
         }
@@ -46,4 +46,4 @@ class JoinNekoCommand extends cmd.Command {
 
 }
 
-module.exports = JoinNekoCommand;
+module.exports = JoinLordCommand;
