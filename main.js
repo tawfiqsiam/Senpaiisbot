0b3f31aceed5ab9c962f4ef9c0abb0bdea83ca2e
tawfiqsiam <toto.siam999@gmail.com>
2020-04-07 22:27:36 +0300

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const embedMessage = `
 `;
 const embedFooter = "Role Reactions"; // Must set this if "embed" is set to true
 const roles = ["Gurl" , "Boi" , "Trans" , "drop pings" , "giveaway ping" , "dead chat ping" , "annoucement ping"];
-const reactions = ["😇" , "😈" , "👤" , "💰" , "🎉" , "😵" , "🔱"]; // For custom emojis, provide the name of the emoji
+const reactions = ["<a:emoji_29:695169244991586364>" , "😈" , "👤" , "💰" , "🎉" , "😵" , "🔱"]; // For custom emojis, provide the name of the emoji
 const embed = true ; // Set to "true" if you want all roles to be in a single embed
 const embedColor = "#dd2423"; // Set the embed color if the "embed" variable is set to true
 const embedThumbnail = true; // Set to "true" if you want to set a thumbnail in the embed
